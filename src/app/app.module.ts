2d55555239e9b3a645a45fa4642c6c07968b39e6
unknown <vibhavraj88@gmail.com>
2020-06-07 16:59:37 +0530

    Custom elements schema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
-import { NavComponent } from './components/nav/nav.component';
-import { BannerComponent } from './components/banner/banner.component';
-import { FooterComponent } from './components/footer/footer.component'
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { NavComponent } from "./components/nav/nav.component";
+import { BannerComponent } from "./components/banner/banner.component";
+import { FooterComponent } from "./components/footer/footer.component";
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent,
-    BannerComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent, NavComponent, BannerComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
