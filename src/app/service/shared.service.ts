9af72fc754d83139fe8ba3ead0310b77c54319b3
unknown <vibhavraj88@gmail.com>
2020-06-08 11:59:29 +0530

    added http headers

--- src/app/service/shared.service.ts
+++ src/app/service/shared.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { HttpHeaders, HttpErrorResponse } from "@angular/common/http";
-
+import { Observable, throwError } from "rxjs";
+import { catchError, retry } from "rxjs/operators";
 const httpOptions = {
   headers: new HttpHeaders({
     "Content-Type": "application/json",
-    Accept: "application/json",
-    "Access-Control-Allow-Origin": "http://localhost:4200",
+    Accept: "application/json, text/plain, */*",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
   }),
 };
 @Injectable({
@@ -17,6 +19,22 @@ export class SharedService {
     "https://cncdigitalgyan.teachable.com/api/v1/hooks/example?event_type=Enrollment.created";
   constructor(private http: HttpClient) {}
   getdata() {
-    return this.http.get(this.url, httpOptions);
+    return this.http.get(this.url, httpOptions).pipe(
+      catchError(this.handleError) // then handle the error
+    );
+  }
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error("An error occurred:", error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // return an observable with a user-facing error message
+    return throwError("Something bad happened; please try again later.");
   }
 }
